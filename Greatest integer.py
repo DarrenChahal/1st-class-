50d7658c550916ec79bf6ff9206f944bb45217af
DarrenChahal <99163411+DarrenChahal@users.noreply.github.com>
2022-11-23 09:57:52 +0530

    Add files via upload

---
+++ Greatest integer.py
@@ -0,0 +1,9 @@
+a = int(input("enter first number "))
+b = int(input("enter first number "))
+c = int(input("enter first number "))
+if a>b and a>c:
+    print ("a")
+if b>a and b>c:
+    print ("b")
+else:
+    print ("c")
